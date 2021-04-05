f27b8c2b0b2e4fec3660fccbe4b09663fe9ff936
Bethany Huhtala-Schoonover <betheth614@gmail.com>
2021-04-05 15:36:22 -0400

    front-end details

---
+++ vue/src/components/SideDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 id="name"></h1>
+    <h2 id="details"></h2>
+    <h2 id="ratings"></h2>
+        <button id="add-review"></button>
+    <h2 id="reviews"></h2>
+  </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    computed: {
+        reviews,
+        ratings
+    },
+    methods: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/components/SideNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="side-nav">
+      <h2>Find Your Beer</h2>
+      <button>Brewery List</button><br>
+      <button>Beer List</button><br>
+      <button>Reviews</button><br>
+      <!-- <button>Favorites</button><br> -->
+      <span>
+        <button>Manage Brewery</button><br>
+        <button>Manage Beers</button><br>
+        <!-- <button>Manage Events</button><br> -->
+      </span>
+      <span>
+        <button>Manage Breweries</button><br>
+        <button>Manage Users</button><br>
+      </span>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
