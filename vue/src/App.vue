f2c99c088b3e34e7d433a0a4cb2e8c68593b9b31
Bethany Huhtala-Schoonover <betheth614@gmail.com>
2021-04-13 17:04:56 -0400

    home logout css

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,21 +1,30 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link id="home-link" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link id="logout-link" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
-
 @media (max-width: 800px) {
   #app{
   flex: 100%;
   flex-direction: column;
   box-sizing: border-box;
-  
 }
+}
+
+#home-link, #logout-link  {
+  text-decoration: none;
+  color: white;
+  padding: .5rem;
+  font-size: 1.3rem;
+}
+
+#home-link:hover, #logout-link:hover {
+  color: rgb(255, 204, 0);
 }
 </style>
