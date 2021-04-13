10c152e3483afafb0b9140821ad5a8ad77a605ad
Charles Kaminski <cvk1997@gmail.com>
2021-04-12 20:26:55 -0400

    beer and brewery image tables, models, and started DAOs

---
+++ API/Capstone/DAO/BeerImageSqlDAO.cs
@@ -0,0 +1,47 @@
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.DAO
+{
+    public class BeerImageSqlDAO : IBeerImagesDAO
+    {
+        private readonly string connectionString;
+
+        public BeerImageSqlDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+        public BeerImages GetImages(int id)
+        {
+            BeerImages returnBeerImages = null;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT beer_img_path FROM beer_images WHERE brewery_id = @id", conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        returnBeerImages.BeerImgPath = Convert.ToString(reader["beer_img_path"]);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return returnBeerImages;
+        }
+
+    }
+}
